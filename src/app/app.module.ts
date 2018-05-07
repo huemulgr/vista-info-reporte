d0f67df312ff3bdce291eb0da181117188a1386f
Jonathan <jona.fer11@gmail.com>
2018-05-06 22:46:41 -0300

    Create base component of Login and Profile.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
